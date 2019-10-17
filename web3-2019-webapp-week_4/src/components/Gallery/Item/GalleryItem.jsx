6c6d497d273301da7a22088d67338254f0c479cc
Michael Nicolaes <michael.nicolaes@student.vinci.be>
2019-10-17 08:28:33 +0200

    feat: editable is done

--- web3-2019-webapp-week_4/src/components/Gallery/Item/GalleryItem.jsx
+++ web3-2019-webapp-week_4/src/components/Gallery/Item/GalleryItem.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 import { Row,Col,Button } from "react-bootstrap";
 import Picture from "../../Picture/Picture";
 import Description from "../../Description/Description";
@@ -12,6 +12,16 @@ function GalleryItem({
     updateItem
 })  {
 
+    const [descriptionHTML, setDescriptionHTML] = useState(description);
+
+    const handleChange = evt => {
+        console.log("Description : ", descriptionHTML);
+        description = evt.target.value;
+        console.log("Description pure : ", description);
+        setDescriptionHTML(evt.target.value);
+    }
+
+
     const deleteItem = (e) => {
         //console.log("Event : ", e);
         //console.log("picture :", {picture});
@@ -19,7 +29,8 @@ function GalleryItem({
     }
 
     const handlerUpdateItem = (e) =>{
-        updateItem({picture, description, id})
+        console.log("Item actual :", e)
+        updateItem({picture, descriptionHTML, id})
     }
    
 
@@ -27,7 +38,7 @@ function GalleryItem({
 
         <Row className="m-2 justify-content-start border border-dark rounded-lg">
             <Picture picture={picture} /> 
-            <Description description={description}/>
+            <Description description={descriptionHTML} handleChange={handleChange}/>
             
             <Col className="col text-center">
                 <Button
