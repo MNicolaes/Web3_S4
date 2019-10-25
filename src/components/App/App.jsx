bb2db667cbbf94f6d6fc765ddb75f8ed4c97a490
Michael Nicolaes <michael.nicolaes@student.vinci.be>
2019-10-25 12:50:03 +0200

    feat: ajout comment pour continuer dans app.js

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,10 +3,18 @@ import { ThemeContext } from '../Context/ThemeContext'
 import './App.css'
 import AppRouter from '../AppRouter'
 
-const {currentTheme, Themes} = useContext(ThemeContext)
+// const {currentTheme, Themes} = useContext(ThemeContext) pour con
 function App() {
+  //currentTheme state
+  //themes array
+  //function setCurrentTheme
+  //Mettre un state UseState pour le current theme et on passe ça au props du themeProvider
+  //On définit aussi la fonction qui va agir sur le current theme ici
+  var valued={theme, themes,setCurrentTheme}
   return (
-    <AppRouter />
+    <ThemeContext value={valued}>
+      <AppRouter />
+    </ThemeContext>
   )
 }
 
